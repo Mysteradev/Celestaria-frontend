30fa3b91695754c1fc046ad3b2627f707ed6b65c
Maxime <maxime.jme@gmail.com>
2020-07-20 23:26:55 +0200

    add store for videos pages

---
+++ store/videos/getters.js
@@ -0,0 +1,8 @@
+export default {
+  videos(state) {
+    return state.videos;
+  },
+  isLoading(state) {
+    return state.isLoading
+  }
+}
---
+++ store/videos/state.js
@@ -0,0 +1,4 @@
+export default () => ({
+  videos: {},
+  isLoading: true
+})
