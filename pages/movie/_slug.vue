39c04823958928e9bb705027a9cddb98eefbc9f8
Kayoshi-dev <maxime.jme@gmail.com>
2021-02-08 21:59:09 +0100

    Fix default state and display default value/api data

--- pages/movie/_slug.vue
+++ pages/movie/_slug.vue
@@ -2,36 +2,40 @@
   <div class="container mt-4">
     <div class="columns">
       <div class="column is-three-quarters">
-        <h3 class="is-size-3 title mb-4">Un film cool <button class="button is-link is-light">Partager !</button></h3>
+        <h3 class="is-size-3 title mb-4">{{ this.getTitle ? this.getTitle : "Votre film"}}<button class="button is-link is-light">Partager !</button></h3>
         <video controls class="movie-player" >
-          <source src="http://techslides.com/demos/sample-videos/small.mp4" type="video/mp4">
+          <source :src="this.getVideoUrl ? this.getVideoUrl : 'http://techslides.com/demos/sample-videos/small.mp4'" type="video/mp4">
         </video>
         <p class="mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam dolore expedita mollitia praesentium provident recusandae velit. Atque corporis dolor dolorum, enim illum ipsum laborum, magnam nihil pariatur, placeat quasi quo?</p>
       </div>
       <div class="column">
-        <verticallistfilm/>
+        <verticalListFilm/>
       </div>
     </div>
   </div>
 </template>
 <script>
 
-import verticallistfilm from "../../components/movie/verticalListFilm";
-import {mapMutations} from "vuex";
+import verticalListFilm from "../../components/movie/verticalListFilm";
+import {mapGetters, mapMutations} from "vuex";
 
 export default {
-  components: {verticallistfilm},
+  components: {verticalListFilm},
   name: "movie",
-  data() {
-    return {
-    }
-  },
   methods: {
     ...mapMutations({
       setTitle: 'movie/SET_TITLE',
       setVideoUrl: 'movie/SET_MOVIE_URL',
       setThumbnailUrl: 'movie/SET_THUMBNAIL_URL',
       setMovieSlug: 'movie/SET_MOVIE_SLUG'
+    })
+  },
+  computed: {
+    ...mapGetters({
+      getTitle: 'movie/getTitle',
+      getIsLoading: 'movie/getIsLoading',
+      getVideoUrl: 'movie/getvideoUrl',
+      getMovieSlug: 'movie/getMovieSlug'
     })
   },
   mounted() {
--- store/movie/state.js
+++ store/movie/state.js
@@ -1,7 +1,7 @@
 export default () => ({
-  title: this.$route.params.title,
+  title: "",
   isLoading: false,
-  thumbnailUrl: this.$route.params.thumbnailUrl,
-  videoUrl : this.$route.params.videoUrl,
-  movieSlug: this.$route.params.slug
+  thumbnailUrl: "",
+  videoUrl : "",
+  movieSlug: ""
 })
